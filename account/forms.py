aacf06b6a10657aa948c671c6c099bda5dc8949c
Biola <52200708+decorouz@users.noreply.github.com>
2022-02-10 23:27:16 +0100

    update registration form

--- account/forms.py
+++ account/forms.py
@@ -1,22 +1,47 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Profile
 
 
-class UserRegistrationForm(forms.ModelForm):
-    password = forms.CharField(label="Password", widget=forms.PasswordInput)
-    password2 = forms.CharField(
-        label="Repeat password", widget=forms.PasswordInput)
+class UserRegistrationForm(UserCreationForm):
+    """ User registration """
+    first_name = forms.CharField(
+        max_length=30, required=False,
+        widget=forms.TextInput(
+            attrs={'class': 'form-control',
+                   'placeholder': 'first name',
+                   'type': 'text',
+                   'id': 'first_name'
+                   }
+        ))
+
+    last_name = forms.CharField(
+        max_length=30,
+        required=False,
+        widget=forms.TextInput(
+            attrs={'class': 'form-control',
+                   'placeholder': 'Last name',
+                   'type': 'text',
+                   'id': 'last_name'
+                   }
+        ))
+
+    email = forms.EmailField(
+        max_length=254,
+        required=True,
+        widget=forms.TextInput(
+            attrs={'class': 'form-control',
+                   'placeholder': 'email address',
+                   'type': 'text',
+                   'id': 'email_address'
+                   }
+        ))
 
     class Meta:
         model = User
-        fields = ("username", "first_name", "email")
-
-    def clean_password2(self):
-        cd = self.cleaned_data
-        if cd["password"] != cd["password2"]:
-            raise forms.ValidationError("Passwords don't match.")
-        cd["password2"]
+        fields = ("username", "first_name", "last_name",
+                  "email", "password1", "password2")
 
 
 class UserEditForm(forms.ModelForm):
