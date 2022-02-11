f19b9bfe779f44639004ea8fad22fad3be19b002
Biola <52200708+decorouz@users.noreply.github.com>
2022-02-11 16:14:58 +0100

    Update register view

--- account/views.py
+++ account/views.py
@@ -1,11 +1,20 @@
-from django.shortcuts import render, redirect
 
+from django.contrib.auth import login
+from django.contrib.sites.shortcuts import get_current_site
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.core.mail import send_mail
+from django.shortcuts import render, redirect
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes, force_str
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from bookmarks import settings
 
 from .forms import UserRegistrationForm, UserEditForm, ProfileEditForm
+from .tokens import account_activation_token
 
 from .models import Profile
+from django.contrib.auth.models import User
 
 
 # Create your views here.
@@ -17,25 +26,73 @@ def dashboard(request):
 def register(request):
     if request.user.is_authenticated:
         return redirect("dashboard")
-
     if request.method == "POST":
         user_form = UserRegistrationForm(request.POST)
         if user_form.is_valid():
-            # create a user object but avoid saving it
+            email = request.POST.get("email")
             new_user = user_form.save(commit=False)
-            # set the choosen password
-            new_user.set_password(
-                user_form.cleaned_data["password"]
-            )
+            new_user.is_active = False
             new_user.save()
-            Profile.objects.create(user=new_user)
-            return render(request, "account/register_done.html", {"new_user": new_user})
+
+            current_site = get_current_site(request)
+
+            subject = "Activate your Account"
+            message = render_to_string("account/account_activation_email.html", {
+                "user": new_user,
+                "domain": current_site.domain,
+                "uid": urlsafe_base64_encode(force_bytes(new_user.pk)),
+                "token": account_activation_token.make_token(new_user)
+
+            })
+
+            try:
+                send_mail(
+                    subject,
+                    message,
+                    settings.EMAIL_HOST_USER,
+                    [email],
+                    fail_silently=False)
+                messages.add_message(request, messages.SUCCESS,
+                                     'A verification email has been sent.')
+                messages.add_message(request, messages.WARNING,
+                                     'Please also check your SPAM inbox!')
+                return render(request,
+                              'account/account_activation_sent.html',
+                              {'new_user': new_user})
+            except Exception as e:
+                print(e)  # e.message
+                messages.add_message(request, messages.WARNING, str(e))
+
     else:
         user_form = UserRegistrationForm()
-    return render(request, "account/register.html", {"user_form": user_form})
-
+    return render(request,
+                  'account/register.html',
+                  {'user_form': user_form})
 
 # create user profile
+
+
+def account_activation_sent(request):
+    return render(request, 'account_activation_sent.html')
+
+
+def activate(request, uidb64, token, backend='django.contrib.auth.backends.ModelBackend'):
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.profile.email_confirmed = True
+        user.save()
+        login(request, user, backend)
+        return redirect('dashboard')
+    else:
+        return render(request, 'account/account_activation_invalid.html')
+
+
 @login_required
 def edit(request):
     if request.method == 'POST':
