f42bf4ca8936a5bcf49fc9d411dba31a15f05d9c
Biola <52200708+decorouz@users.noreply.github.com>
2022-02-11 16:14:22 +0100

    Profile create signal

--- account/signals.py
+++ account/signals.py
@@ -2,28 +2,22 @@ from django.db.models.signals import post_save
 from .models import Profile
 from django.contrib.auth.models import User
 from django.dispatch import receiver
-from django.core.mail import send_mail
-from django.conf import settings
 
 
 @receiver(post_save, sender=User)
-def create_profile(sender, instance, created, **kwargs):
-    """Receiver function"""
+def update_user_profile(sender, instance, created, **kwargs):
     if created:
-        new_user = instance
-        profile = Profile.objects.create(
-            user=new_user
-
-        )
+        Profile.objects.create(user=instance)
+    instance.profile.save()
 
 
-def update_user(sender, instance, created, **kwargs):
-    profile = instance
-    user = profile.user
+# def update_user(sender, instance, created, **kwargs):
+#     profile = instance
+#     user = profile.user
 
-    if created == False:
+#     if created == False:
 
-        user.save()
+#         user.save()
 
 
-post_save.connect(update_user, sender=Profile)
+# post_save.connect(update_user, sender=Profile)
