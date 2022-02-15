e1cd110e934ae36c088ceb0f853584b8b740c4c7
Biola <52200708+decorouz@users.noreply.github.com>
2022-02-15 17:23:07 +0100

    Create Contact Model

--- account/models.py
+++ account/models.py
@@ -14,3 +14,17 @@ class Profile(models.Model):
     def __str__(self) -> str:
         return f"Profile for user {self.user.username}"
 # Create your models here.
+
+
+class Contact(models.Model):
+    user_from = models.ForeignKey(
+        "auth.User", related_name="rel_from_set", on_delete=models.CASCADE)
+    user_to = models.ForeignKey(
+        "auth.User", related_name="rel_to_set", on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True, db_index=True)
+
+    class Meta:
+        ordering = ("-created")
+
+    def __str__(self) -> str:
+        return f"{self.user_from} follows {self.user_from}"
