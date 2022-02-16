b665a5b92b989924e0684eec37487b7f2de15a6d
Biola <52200708+decorouz@users.noreply.github.com>
2022-02-16 18:26:59 +0100

    Complete activity stream

--- images/views.py
+++ images/views.py
@@ -76,7 +76,7 @@ def image_like(request):
 @login_required
 def image_list(request):
     images = Image.objects.all()
-    paginator = Paginator(images, 15)
+    paginator = Paginator(images, 13)
     page = request.GET.get("page")
 
     try:
